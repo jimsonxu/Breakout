450cfdc3239e763706005223299844b49cb8b374
Jimson Xu <jimson.xu@gmail.com>
2016-01-29 16:41:53 -0800

    Create a simple bouncing ball
    Disable gravity. Add an initial force to the ball to get it falling down. Add a bouncy material of bounciness 1 to the ball.

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	[SerializeField]
+	float force = 4.5f;
+
+	Rigidbody2D myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D> ();
+		myRigidBody.AddForce (new Vector2(0, -force));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myRigidBody.AddForce (new Vector2(0, -force));
+	}
+}
