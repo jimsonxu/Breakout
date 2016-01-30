84ae50820fbbc4ef4183f3ae877b31d06f844d36
Jimson Xu <jimson.xu@gmail.com>
2016-01-29 16:52:10 -0800

    Arrow keys move the paddle
    Attach a script to the paddleSprite. Left and Right arrow keys move the paddle.

---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleScript : MonoBehaviour {
+	
+	Transform myTransform;
+
+	[SerializeField]
+	float speed = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 curPos = myTransform.position;
+		if (Input.GetKey ("left")) {
+			myTransform.position = new Vector2 (curPos.x - speed, curPos.y);
+		} else if (Input.GetKey ("right")) {
+			myTransform.position = new Vector2 (curPos.x + speed, curPos.y);
+		}
+	}
+}
