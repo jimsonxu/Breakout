49f59cc9362c2bc1b93b87474c92e8dc36094601
Jimson Xu <jimson.xu@gmail.com>
2016-02-01 11:51:00 -0800

    Alternate brick colors
    Why? For fun. For aesthetics. And so we can distinguish the individual bricks better.

--- Assets/Scripts/BrickManager.cs
+++ Assets/Scripts/BrickManager.cs
@@ -8,9 +8,21 @@ public class BrickManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Color colorOdd = Color.white;
+		Color colorEven = Color.cyan;
+		SpriteRenderer renderer = m_brickPrefab.GetComponent<SpriteRenderer>();
+		renderer.color = colorOdd;
+		int cnt = 1;
+
 		for (int x = 0; x < 5; x++) {
 			for (int y = 0; y < 5; y++) {
+				if (cnt % 2 == 0)
+					renderer.color = colorEven;
+				else
+					renderer.color = colorOdd;
+				
 				Instantiate (m_brickPrefab, new Vector2 (x, y), Quaternion.identity);
+				cnt++;
 			}
 		}
 	}
