cf8376705215195d0dc44567dc65b364b834bbb0
Jimson Xu <jimson.xu@gmail.com>
2016-02-01 11:04:55 -0800

    Programmatically generate bricks on the scene
    Add a BrickManager GameObject that programmatically generates bricks on the scene.

---
+++ Assets/Scripts/BrickManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickManager : MonoBehaviour {
+
+	[SerializeField]
+	Transform m_brickPrefab;
+
+	// Use this for initialization
+	void Start () {
+		for (int x = 0; x < 5; x++) {
+			for (int y = 0; y < 5; y++) {
+				Instantiate (m_brickPrefab, new Vector2 (x, y), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
