4111ef216190c8ef5abeafe4134c3b94824f3b76
Jimson Xu <jimson.xu@gmail.com>
2016-02-01 10:09:59 -0800

    Make paddle affect ball direction
    Based off the position the ball hits the paddle, calculate the new direction. The further from the center of the paddle, the sharper the angle. Effectively, when the ball hits the left side of the paddle, the ball moves left. When the ball hits the right side, it moves right.
    
    This does not take into account the velocity of the ball, only its position when it hits the paddle.

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -7,10 +7,24 @@ public class BallScript : MonoBehaviour {
 	float m_speed = 5;
 
 	Rigidbody2D m_rigidBody;
+	Collider2D m_collider;
 
 	// Use this for initialization
 	void Start () {
 		m_rigidBody = GetComponent<Rigidbody2D> ();
 		m_rigidBody.velocity = Vector2.down * m_speed;
+
+		m_collider = GetComponent<Collider2D> ();
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.name == "Paddle") {
+			/* Angle is determined by the ball's distance from center of paddle. Greater the distance,
+			 * sharper the angle (smaller angle).
+			 */
+			float ballToPaddleX = transform.position.x - col.transform.position.x;
+			Vector2 newDir = new Vector2 (ballToPaddleX, 1).normalized;
+			m_rigidBody.velocity = newDir * m_rigidBody.velocity.magnitude;
+		}
 	}
 }
