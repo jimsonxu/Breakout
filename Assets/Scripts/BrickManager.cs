1f0fa412d22ed00fa8d265d3c8956f1ea8a06b50
Jimson Xu <jimson.xu@gmail.com>
2016-02-01 17:08:18 -0800

    Read brick size from config file
    Add bricks.cfg to the Assets folder. This contains the brick configuration information.
    BrickManager now reads this config file on startup.
    
    config file syntax so far:
    size=<x>,<y>
    
    Where x and y are the length and height of the rectangle (brick).

--- Assets/Scripts/BrickManager.cs
+++ Assets/Scripts/BrickManager.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
+using System;
 
 public class BrickManager : MonoBehaviour {
 
 	[SerializeField]
 	Transform m_brickPrefab;
 
+	Vector2 m_brickSize;
+
 	// Use this for initialization
 	void Start () {
+		LoadBricksCfg ();
+
 		Color colorOdd = Color.white;
 		Color colorEven = Color.cyan;
 		SpriteRenderer renderer = m_brickPrefab.GetComponent<SpriteRenderer>();
@@ -30,5 +36,47 @@ public class BrickManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	/***
+	 * Valid config line types:
+	 * size=2,1
+	 * [empty line]
+	 * 1,1
+	 * 
+	 * Returns true on successful load and false on failure
+	 */
+	bool LoadBricksCfg() {
+		try {
+			TextReader reader = new StreamReader ("Assets/bricks.cfg");
+
+			using (reader) {
+				string line = reader.ReadLine();
+				string[] values, coord;
+				int x, y;
+
+				while (line != null) {
+					values = line.Split('=');
+					if (values.Length > 0) {
+						if (values[0] == "size") {
+							coord = values[1].Split(',');
+							x = Int32.Parse(coord[0]);
+							y = Int32.Parse(coord[1]);
+
+							m_brickSize = new Vector2(x, y);
+						}
+					}
+
+					line = reader.ReadLine ();
+				}
+
+				reader.Close();
+				return true;
+			}
+		}
+		catch (Exception e) {
+			Debug.Log (e.Message);
+			return false;
+		}
 	}
 }
