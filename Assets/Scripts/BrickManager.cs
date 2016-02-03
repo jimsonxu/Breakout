5606ee27b72e31f836e259b7416f6921f1db43d2
Jimson Xu <jimson.xu@gmail.com>
2016-02-03 14:47:43 -0800

    Parse and spawn power bricks
    Add new tag "Power".
    Refactor vector2 parsing of the config file into its own function ParseVector2.
    Add the ability to parse power brick coordinates from the config file. And spawn the power brick at the correct location. Must follow this format: "pow <x>,<y>", where there must be no spaces in the coordinate.

--- Assets/Scripts/BrickManager.cs
+++ Assets/Scripts/BrickManager.cs
@@ -9,6 +9,10 @@ public class BrickManager : MonoBehaviour {
 	const float kCfgPixel = 0.5f; // 1 cfg pixel is 0.5 game pixels
 	Vector2 kDefaultScale;
 
+	bool kPowOn = true;
+	const string kPowerTagString = "Power";
+	Color kPowerColor;
+
 	[SerializeField]
 	Transform m_brickPrefab;
 
@@ -19,6 +23,9 @@ public class BrickManager : MonoBehaviour {
 	void Start () {
 		kTopLeft = new Vector2 (-6.87f, 4.6f);
 		kDefaultScale = new Vector2 (0.75f, 0.25f);
+		kPowerColor = Color.yellow;
+		m_brickSize = Vector2.one;
+
 		m_renderer = m_brickPrefab.GetComponent<SpriteRenderer> ();
 		LoadBricksCfg ();
 	}
@@ -33,6 +40,7 @@ public class BrickManager : MonoBehaviour {
 	 * size=2,1
 	 * [empty line]
 	 * 1,1
+	 * pow 1,1
 	 * 
 	 * Returns true on successful load and false on failure
 	 */
@@ -42,29 +50,30 @@ public class BrickManager : MonoBehaviour {
 
 			using (reader) {
 				string line = reader.ReadLine();
-				string[] values, coord;
-				int x, y;
+				string[] values;
+				Vector2 pos;
 
 				while (line != null) {
 					values = line.Split('=');
 					if (values.Length > 0) {
 						if (values[0] == "size") {
-							coord = values[1].Split(',');
-							x = Int32.Parse(coord[0]);
-							y = Int32.Parse(coord[1]);
-
-							m_brickSize = new Vector2(x, y);
+							pos = ParseVector2(values[1]);
+							if (pos.x >= 1 && pos.y >= 1) m_brickSize = pos;
 
 							m_renderer.transform.localScale = new Vector3(m_brickSize.x * kDefaultScale.x, m_brickSize.y * kDefaultScale.y, 0);
 						} else if (values.Length == 1 && values[0].Length > 0) {
 							// no '=' found and not a blank line
-							// brick coordinate
-							coord = line.Split(',');
-							if (coord.Length > 0) {
-								x = Int32.Parse(coord[0]);
-								y = Int32.Parse(coord[1]);
 
-								SpawnBrick(new Vector2(x, y));
+							values = line.Split(' ');
+							if (values.Length > 1 && values[0] == "pow") {
+								pos = ParseVector2(values[1]);
+								if (pos.x >= 0 && pos.y >= 0) SpawnBrick(pos, kPowOn);
+							}
+							else {
+								// a bit fragile right now, cannot have spaces in the coordinates
+								// brick coordinate
+								pos = ParseVector2(line);
+								if (pos.x >= 0 && pos.y >= 0) SpawnBrick(pos);
 							}
 						}
 					}
@@ -82,7 +91,25 @@ public class BrickManager : MonoBehaviour {
 		}
 	}
 
-	void SpawnBrick(Vector2 pos) {
+	// vector must have the format: <number>,<number>
+	// no spaces allowed!
+	//
+	// This will parse the string and return a Vector x,y with the values filled in.
+	// Returns -1, -1 on error, as this is an invalid config file value.
+	Vector2 ParseVector2(string v) {
+		string[] arr = v.Split(',');
+		int x = -1;
+		int y = -1;
+
+		if (arr.Length > 1) {
+			x = Int32.Parse (arr [0]);
+			y = Int32.Parse (arr [1]);
+		}
+
+		return new Vector2 (x, y);
+	}
+
+	void SpawnBrick(Vector2 pos, bool powOn = false) {
 		// convert config coordinate to game world coordinates
 		Vector2 gamePos = new Vector2(kTopLeft.x + (pos.x * kCfgPixel), + kTopLeft.y - (pos.y * kCfgPixel));
 
@@ -92,6 +119,12 @@ public class BrickManager : MonoBehaviour {
 		gamePos.y -= m_renderer.bounds.extents.y;
 
 		m_renderer.color = Color.cyan;
-		Instantiate (m_brickPrefab, gamePos, Quaternion.identity);
+		Transform newObj = Instantiate (m_brickPrefab, gamePos, Quaternion.identity) as Transform;
+
+		if (powOn) {
+			newObj.tag = kPowerTagString;
+			SpriteRenderer renderer = newObj.GetComponent<SpriteRenderer> ();
+			renderer.color = kPowerColor;
+		}
 	}
 }
